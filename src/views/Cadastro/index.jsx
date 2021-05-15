f2878a1a59518eeb19bb1d427ca8688abd9d8f59
guilherme terriaga <guilherme.terriaga@gmail.com>
2021-05-14 22:29:02 -0300

    Configurada a API do Axios e cadastro funcional.

--- src/views/Cadastro/index.jsx
+++ src/views/Cadastro/index.jsx
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from '../../services/api';
 import { Link } from 'react-router-dom';
 import './style/cadastro.css';
 
+//verficar a questão da foto do usuário pelo backend se não der para o usuário colocar
+//foto antes de criar o perfil
+
 function Cadastro() {
+  const [apelido, setApelido] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
   return (
     <div className="body">
       <div className="cadastro-box">
         <h2>Cadastro</h2>
         <form>
           <div className="user-box">
-            <input type="text" name="" required="" />
+            {/* <input type="text" name="apelido" required="" /> */}
+            <input
+              type="text"
+              name="apelido"
+              required=""
+              value={apelido}
+              onChange={(e) => setApelido(e.target.value)}
+            />
             <label>Apelido</label>
           </div>
           <div className="user-box">
-            <input type="email" name="" required="" />
+            {/* <input type="email" name="" required="" /> */}
+            <input
+              type="email"
+              name=""
+              required=""
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
             <label>Email</label>
           </div>
           <div className="user-box">
-            <input type="password" name="" required="" />
+            {/* <input type="password" name="" required="" /> */}
+            <input
+              type="password"
+              name=""
+              required=""
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+            />
+
             <label>Senha</label>
           </div>
           <Link to="/login">
@@ -27,7 +58,7 @@ function Cadastro() {
             <span></span>
             Entrar
           </Link>
-          <Link to="/login" className="btnCadastrar">
+          <Link to="#" className="btnCadastrar" onClick={novoUsuario}>
             <span></span>
             <span></span>
             <span></span>
@@ -38,6 +69,24 @@ function Cadastro() {
       </div>
     </div>
   );
+
+  async function novoUsuario() {
+    await api
+      .post('users', {
+        apelido: apelido,
+        email: email,
+        senha: senha,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    setApelido('batata');
+    setEmail('batatinha');
+    setSenha('batatão');
+  }
 }
 
 export default Cadastro;
