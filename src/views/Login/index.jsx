80a7dcfb1714af326015056c1a1a3df75e700710
guilherme terriaga <guilherme.terriaga@gmail.com>
2021-05-18 22:05:39 -0300

    login ok

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './style/login.css';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import api from '../../services/api';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
   return (
     <div className="body">
-      <div className="login-box">
+      <div className="cadastro-box">
         <h2>Login</h2>
         <form>
           <div className="user-box">
-            <input type="text" name="" required="" />
+            <input
+              type="text"
+              name="email"
+              pattern="/^[^\s@]+@[^\s@]+$/"
+              required=""
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <label>Usuário</label>
           </div>
           <div className="user-box">
-            <input type="password" name="" required="" />
+            <input
+              type="password"
+              name="senha"
+              required=""
+              pattern="[^\0]"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+            />
             <label>Senha</label>
           </div>
           <Link className="esqueci-senha" to="#">
             esqueci a senha
           </Link>
-          <Link to="/perfil">
+          <Link to="#" onClick={loginUsuario}>
             <span></span>
             <span></span>
             <span></span>
@@ -37,6 +54,23 @@ function Login() {
       </div>
     </div>
   );
+
+  async function loginUsuario() {
+    await api
+      .post('sessions', {
+        email: email,
+        senha: senha,
+      })
+      .then(function (response) {
+        console.log(response);
+        // Redirect('/perfil');
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    setEmail('');
+    setSenha('');
+  }
 }
 
 export default Login;
