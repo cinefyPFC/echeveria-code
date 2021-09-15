34609158c7fc53a897d0f540e06e1304b88afaa4
Guilherme Terriaga <guilherme.terriaga@gmail.com>
2021-09-14 23:03:40 -0300

    agora compila e aparentemente funciona

--- src/components/HeaderAdmin/index.jsx
+++ src/components/HeaderAdmin/index.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import './header.css';
-import { Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { FiHome, FiUser, FiSettings } from 'react-icons/fi';
+import { Col } from 'reactstrap';
+import './header.css';
+// import { FiHome, FiUser, FiSettings } from 'react-icons/fi';
 
 export default function HeaderAdmin() {
   return (
     <Col md={2} className="sidebar">
       <Link to="/dashboard">
-        <FiHome color="#FFF" size={24} />
+        {/* <FiHome color="#FFF" size={24} /> */}
         Inicio
       </Link>
       <Link to="/dashboard/usuarios">
-        <FiUser color="#FFF" size={24} />
+        {/* <FiUser color="#FFF" size={24} /> */}
         Usuário
       </Link>
       <Link to="#">
-        <FiSettings color="#FFF" size={24} />
+        {/* <FiSettings color="#FFF" size={24} /> */}
         Configurações
       </Link>
     </Col>
