6979a67f22dd51c2c3709227ab4eaeaadabdcd59
David <dkaortiz@gmail.com>
2021-09-17 19:47:43 -0300

    tabelas para testar

--- src/views/GerenciamentoUsuario/index.jsx
+++ src/views/GerenciamentoUsuario/index.jsx
@@ -1,53 +1,87 @@
 import React from 'react';
 import HeaderAdmin from '../../components/HeaderAdmin';
-import { Table } from 'reactstrap';
+import { Button, Table } from 'reactstrap';
 import './style/gerenciamento.css';
+import { FcEmptyTrash,FcAbout } from "react-icons/fc";
+import { UncontrolledButtonDropdown, DropdownMenu, DropdownItem, DropdownToggle } from 'reactstrap';
+
 
 function Usuario() {
   return (
+
     <div className="AdminTela">
       <HeaderAdmin></HeaderAdmin>
       <div className="AdminConteudo">
-        <Table dark>
+
+        <Table dark striped responsive>
           <thead>
             <tr>
               <th>#</th>
-              <th>Apelido</th>
+              <th>Nome</th>
               <th>Email</th>
-              <th>Username</th>
-              <th>Personagem Favorito</th>
               <th>Ações</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <th scope="row">1</th>
-              <td className="table-gerenciamento-usuario">Prettier lixo</td>
-              <td className="table-gerenciamento-usuario">Prettier lixo</td>
-              <td className="table-gerenciamento-usuario">Prettier lixo</td>
-              <td className="table-gerenciamento-usuario">Prettier lixo</td>
-              <td className="table-gerenciamento-usuario">Prettier lixo</td>
+              <td className="table-gerenciamento-usuario">Guilherme</td>
+              <td className="table-gerenciamento-usuario">guilherme@alunos.umc.br</td>
+              <td className="table-gerenciamento-usuario">
+              <UncontrolledButtonDropdown>
+                <DropdownToggle caret>
+                   ...
+                </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem> <FcEmptyTrash></FcEmptyTrash>  Excluir</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem><FcAbout></FcAbout> Editar</DropdownItem>
+                </DropdownMenu>
+            </UncontrolledButtonDropdown>
+                </td>
             </tr>
             <tr>
               <th scope="row">2</th>
-              <td className="table-gerenciamento-usuario">Prettier lixo</td>
-              <td className="table-gerenciamento-usuario">Prettier lixo</td>
-              <td className="table-gerenciamento-usuario">Prettier lixo</td>
-              <td className="table-gerenciamento-usuario">Prettier lixo</td>
-              <td className="table-gerenciamento-usuario">Prettier lixo</td>
+              <td className="table-gerenciamento-usuario">Kelvin</td>
+              <td className="table-gerenciamento-usuario">kelv@umc.br</td>
+              <td className="table-gerenciamento-usuario">
+
+              <UncontrolledButtonDropdown>
+                <DropdownToggle caret>
+                   ...
+                </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem> <FcEmptyTrash></FcEmptyTrash>  Excluir</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem><FcAbout></FcAbout> Editar</DropdownItem>
+                </DropdownMenu>
+            </UncontrolledButtonDropdown>
+                </td>
             </tr>
             <tr>
               <th scope="row">3</th>
-              <td className="table-gerenciamento-usuario">Prettier lixo</td>
-              <td className="table-gerenciamento-usuario">Prettier lixo</td>
-              <td className="table-gerenciamento-usuario">Prettier lixo</td>
-              <td className="table-gerenciamento-usuario">Prettier lixo</td>
-              <td className="table-gerenciamento-usuario">Prettier lixo</td>
+              <td className="table-gerenciamento-usuario">Micael</td>
+              <td className="table-gerenciamento-usuario">micael@umc.br</td>
+              <td className="table-gerenciamento-usuario">
+
+              <UncontrolledButtonDropdown>
+                <DropdownToggle caret>
+                   ...
+                </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem> <FcEmptyTrash></FcEmptyTrash>  Excluir</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem><FcAbout></FcAbout> Editar</DropdownItem>
+                </DropdownMenu>
+            </UncontrolledButtonDropdown>
+
+              </td>
             </tr>
           </tbody>
         </Table>
       </div>
     </div>
+
   );
 }
 export default Usuario;
