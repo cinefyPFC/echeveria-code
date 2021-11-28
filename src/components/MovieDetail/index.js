11eefda2c242f2ac59fbcba7b05c5b5f7ea250fb
Guilherme Terriaga <guilherme.terriaga@gmail.com>
2021-11-28 15:49:53 -0300

    apostrofo erro

--- src/components/MovieDetail/index.js
+++ src/components/MovieDetail/index.js
@@ -244,7 +244,7 @@ function MovieDetail(props) {
           <h4 className="user-coments=name">Fanta Sabor original</h4>
           <p className="user-coments">
             Lorem Ipsum is simply dummy text of the printing and
-            typesetting industry. Lorem Ipsum has been the industry's standard
+            typesetting industry. Lorem Ipsum has been the industry&aposs standard
             dummy text ever since the 1500s, when an unknown printer took a
             galley of type and scrambled it to make a type specimen book. It has
             survived not only five centuries, but also the leap into electronic
